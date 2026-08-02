a4284c1f76fbd2f024af5ac1cf4592466f7b9e5a
Malek Chihi <chihimalek1234@gmail.com>
2026-08-02 16:11:36 +0100

    Add files via upload

--- firmware/tests/blink_test/blink_test.ino
+++ firmware/tests/blink_test/blink_test.ino
@@ -1,1 +1,13 @@
+#define LED_PIN 8     // LED branchée sur GPIO8
+#define BLINK_DELAY 500  // Délai en ms (0,5s allumé, 0,5s éteint)
 
+void setup() {
+  pinMode(LED_PIN, OUTPUT);   // Définit GPIO8 en sortie
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);  // LED ON
+  delay(BLINK_DELAY);           // Attend 500 ms
+  digitalWrite(LED_PIN, LOW);   // LED OFF
+  delay(BLINK_DELAY);           // Attend 500 ms
+}
